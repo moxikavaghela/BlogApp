845346834f7c64c15fb3ce6eaea138e20ba84963
Moxika Vaghela <moxikavaghela15123@gmail.com>
2025-05-09 12:26:57 +0530

    Adding ajex request for adding new comments

--- MoxikaBlogApp/Controllers/PostController.cs
+++ MoxikaBlogApp/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MoxikaBlogApp.Data;
+using MoxikaBlogApp.Models;
 using MoxikaBlogApp.Models.ViewModels;
 using System.Net;
 
@@ -12,25 +13,25 @@ namespace MoxikaBlogApp.Controllers
     {
         private readonly AppDbContext _context;
         private readonly IWebHostEnvironment _webHostEnvironment;
-        private readonly string[] _allowedExtension ={".jpg",".jpeg",".png"};
+        private readonly string[] _allowedExtension = { ".jpg", ".jpeg", ".png" };
 
-        public PostController(AppDbContext context, IWebHostEnvironment webHostEnvironment) 
+        public PostController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
         {
-            _context=context;
-            _webHostEnvironment=webHostEnvironment;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpGet]
         public IActionResult Index(int? categoryId)
         {
-            var postQuery = _context.Posts.Include(p =>p.Category).AsQueryable();
+            var postQuery = _context.Posts.Include(p => p.Category).AsQueryable();
 
-            if (categoryId.HasValue) 
+            if (categoryId.HasValue)
             {
                 postQuery = postQuery.Where(p => p.CategoryId == categoryId);
             }
             var posts = postQuery.ToList();
-            ViewBag.Categories =_context.Categories.ToList();
+            ViewBag.Categories = _context.Categories.ToList();
             return View(posts);
         }
 
@@ -77,9 +78,9 @@ namespace MoxikaBlogApp.Controllers
         public IActionResult Create()
         {
             var postViewModel = new PostViewModel();
-            postViewModel.Categories = _context.Categories.Select(c=>
-            new SelectListItem 
-            {                
+            postViewModel.Categories = _context.Categories.Select(c =>
+            new SelectListItem
+            {
                 Value = c.Id.ToString(),
                 Text = c.Name
             }
@@ -95,13 +96,13 @@ namespace MoxikaBlogApp.Controllers
             {
                 var inputFileExtension = Path.GetExtension(postViewModel.FeatureImage.FileName).ToLower();
                 bool isAllowed = _allowedExtension.Contains(inputFileExtension);
-                if (!isAllowed) 
+                if (!isAllowed)
                 {
                     ModelState.AddModelError("", "Only .jpg, .jpeg, .png files are allowed");
                     return View(postViewModel);
                 }
 
-               postViewModel.Post.FeatureImagePath = await UploadFiletoFolder(postViewModel.FeatureImage);
+                postViewModel.Post.FeatureImagePath = await UploadFiletoFolder(postViewModel.FeatureImage);
                 await _context.Posts.AddAsync(postViewModel.Post);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
@@ -119,11 +120,33 @@ namespace MoxikaBlogApp.Controllers
 
         }
 
+        public JsonResult AddComment([FromBody]Comments comment)
+        {
+            comment.CommentDate = DateTime.Now;
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+
+            return Json(new
+            {
+                UserName = comment.UserName, // Capital "N"
+                CommentDate = comment.CommentDate.ToString("MMM dd yyyy"),
+                Content = comment.Content
+            });
+
+
+            //return Json(new
+            //{
+            //    Username = comment.UserName,
+            //    CommentDate = comment.CommentDate.ToString("MMM dd yyyy"),
+            //    Content = comment.Content
+            //});
+        }
+
         private async Task<string> UploadFiletoFolder(IFormFile file)
         {
             var inputFileExtension = Path.GetExtension(file.FileName);
-            var fileName = Guid.NewGuid().ToString()+ inputFileExtension;
-            var wwwRootPath= _webHostEnvironment.WebRootPath;
+            var fileName = Guid.NewGuid().ToString() + inputFileExtension;
+            var wwwRootPath = _webHostEnvironment.WebRootPath;
             var ImagesFolderPath = Path.Combine(wwwRootPath, "images");
 
             if (!Directory.Exists(ImagesFolderPath))
@@ -132,9 +155,9 @@ namespace MoxikaBlogApp.Controllers
             }
 
             var filePath = Path.Combine(ImagesFolderPath, fileName);
-            try 
+            try
             {
-                await using(var fileStream = new FileStream(filePath, FileMode.Create))
+                await using (var fileStream = new FileStream(filePath, FileMode.Create))
                 {
                     await file.CopyToAsync(fileStream);
                 }
@@ -143,7 +166,7 @@ namespace MoxikaBlogApp.Controllers
             catch (Exception ex)
             {
                 //ModelState.AddModelError("", "File upload failed");
-                return "Error Uploading Image: "+ex.Message;
+                return "Error Uploading Image: " + ex.Message;
             }
             return "/images/" + fileName;
         }
